639718e117b4ced39f97a59a41aa9fb9415bf3a4
Índigo Andre Ortiz Vega <inanortizve@ittepic.edu.mx>
2026-03-09 23:46:20 -0700

    Admin

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import Sidebar from './components/sidebar';
+import Sidebar from '../components/sidebar';
 import '/usuarios.css';
 import '/admin.css';
 
